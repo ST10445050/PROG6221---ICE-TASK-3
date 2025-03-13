39b2b00116b8c5f51d9108303ebfad7d5afbac2c
Keona Mackan <ST10445050@vcconnect.edu.za>
2025-03-13 19:11:52 +0200

    Last Commit

--- IceTask3/Program.cs
+++ IceTask3/Program.cs
@@ -4,19 +4,20 @@
     {
         static void Main(string[] args)
         {
-            // Create an instance of Rectangle
-            Shape rectangle = new Rectangle("Rectangle", 5, 10);
+            // Creating an instance of Rectangle
+            Rectangle r = new Rectangle("Rectangle", 5, 3); 
+            r.Display();  
 
             Console.WriteLine();
-            Console.WriteLine("********************************");
+            Console.WriteLine("*******************************");
             Console.WriteLine();
 
 
-            // Create an instance of Circle
-            Shape circle = new Circle("Circle", 7);
-            circle.Display();
+           // Creating an instance of Circle
+            Circle c = new Circle("Circle", 5);  
+            c.Display();  // Display the circle's info and area
 
-            Console.ReadKey();
+            Console.ReadKey(); 
         }
     }
 }
