6acc663e3a95f24b5ceb73114d767c17205b77dc
Keona Mackan <ST10445050@vcconnect.edu.za>
2025-03-13 18:12:46 +0200

    ICalculateArea Interface

---
+++ IceTask3/ICalculateArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask3
+{
+    public interface ICalculateArea
+    {
+      // Method to calculate area
+        double CalculateArea(); 
+    }
+
+}
