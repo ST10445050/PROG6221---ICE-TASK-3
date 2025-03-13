027ef6d869189172985b0ed9d882a8cd46f6113e
Keona Mackan <ST10445050@vcconnect.edu.za>
2025-03-13 18:26:56 +0200

    Rectangle Class

---
+++ IceTask3/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask3
+{
+    public class Rectangle : Shape, ICalculateArea
+    {
+        // Properties to store Length and Width
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        // Custom Constructor 
+        public Rectangle(string name, double length, double width) : base(name)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        // Implementing CalculateArea from ICalculateArea
+        public double CalculateArea()  
+        {
+            double area = Length * Width;  
+            return area;  
+        }
+
+        // Overriding the Display method
+        public override void Display()
+        {
+            Console.WriteLine($"Shape: {Name}");
+            Console.WriteLine($"Length: {Length}, Width: {Width}");
+            double area = CalculateArea();  
+            Console.WriteLine($"Area of the Rectangle is: {area}");  
+        }
+    }
+
+}
+
+
+
+
