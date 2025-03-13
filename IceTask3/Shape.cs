72a5d375e85dd97272e37f078208822346f4962b
Keona Mackan <ST10445050@vcconnect.edu.za>
2025-03-13 18:14:44 +0200

    Shape Class

---
+++ IceTask3/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask3
+{
+   public abstract class Shape
+    {
+        // Property to store Name
+        public string Name { get; set; }  
+
+        // Constructor 
+        public Shape(string name)
+        {
+            this.Name = name;
+        }
+
+        // Virtual Display Method
+        public virtual void Display()
+        {
+            Console.WriteLine($"Shape: {Name}");
+        }
+    }
+
+}
